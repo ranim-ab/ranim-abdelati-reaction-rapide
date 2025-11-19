1b7ab45a324a505be73b4e3f5aadfc666b91df62
ranim-ab <ranim.abdelati@etudiant-fst.utm.tn>
2025-11-19 22:31:03 +0100

    final final commit

--- projet_dsv.js
+++ projet_dsv.js
@@ -30,24 +30,23 @@ function Commencer_v() {
     btn.addEventListener("click", Clic_v);
 }
 
+
 function Clic_v() {
     if (tm1 === 0) {
         wait = true;
         console.log("click1");
         btn.style.backgroundColor = "rgb(27, 91, 187)";
         btn.innerHTML = "Attendez le rouge...";
-        tm1 = 1; 
-        i = 0;
-        moy = 0;
+        tm1++;//bch yetaada toul lel click2
 
         to = setTimeout(() => {
             wait = false; 
             btn.style.backgroundColor = "red";
-            now = performance.now(); 
-            console.log(now);
+            now = performance.now();
         }, 1000 + Math.random() * 3000);
 
         btn.addEventListener("click", Tot_v);
+        
     }
     
     else if (wait===false) {
@@ -55,11 +54,11 @@ function Clic_v() {
         let reaction = performance.now() - now;
         moy=moy+reaction;
         console.log(reaction);
+        btn.removeEventListener("click", Tot_v);//sinon ca s'execute apres le resultat à chaque fois
         btn.innerHTML = Math.round(reaction) + " ms<br>Cliquez pour recommencer!";
-        
         btn.style.backgroundColor = "rgba(101, 177, 238, 1)";
-        btn.style.padding = "70px 160px";
-        tm1 = 0;
+
+        tm1 =0;
         i++;
         console.log(i);
         endgame(i, Clic_v, Tot_v);
@@ -70,12 +69,11 @@ function Tot_v() {
     if(wait){
         console.log("trop tot");
         clearTimeout(to);
-        alert("Oops ! Trop tôt, recommencez !");
-
+        btn.innerHTML = "Oops ! Trop tôt, recommencez!";
         btn.style.backgroundColor = "rgba(101, 177, 238, 1)";
-        btn.innerHTML = "Cliquez pour recommencer!";
         wait = false;
         tm1 = 0;
+        btn.removeEventListener("click", Tot_v);
     }
     
 }
@@ -86,7 +84,6 @@ btna.addEventListener("click", () => {
     console.log("restarta");
     btn.style.backgroundColor = "rgb(27, 91, 187)";
     btn.innerHTML = "Cliquez pour commencer !";
-    let i=0;
     Commencer_a();
 });
 
@@ -111,8 +108,6 @@ function Clic_a() {
         btn.style.backgroundColor = "rgb(27, 91, 187)";
         btn.innerHTML = "Attendez le signal sonore...";
         tm1 = 1; 
-        i = 0;
-        moy = 0;
 
         to = setTimeout(() => {
             wait = false; 
@@ -128,10 +123,10 @@ function Clic_a() {
         let reaction = performance.now() - now;
         moy=moy+reaction;
         console.log(reaction);
-
+        btn.removeEventListener("click", Tot_a);
         btn.innerHTML = Math.round(reaction) + " ms<br>Cliquez pour recommencer!";
         btn.style.backgroundColor = "rgba(101, 177, 238, 1)";
-        btn.style.padding = "70px 160px";
+
         tm1 = 0;
         i++;
         console.log(i);
@@ -142,28 +137,37 @@ function Tot_a() {
     if(wait){
         console.log("trop tot");
         clearTimeout(to);
-        alert("Oops ! Trop tôt, recommencez !");
-        
+        btn.innerHTML = "Oops ! Trop tôt, recommencez!";
         btn.style.backgroundColor = "rgba(101, 177, 238, 1)";
-        btn.innerHTML = "Cliquez pour recommencer!";
         wait = false;
         tm1 = 0;
+        btn.removeEventListener("click", Tot_a);
     }
 }
 function endgame(i, clic, tot) {
     try{
-        if(i==1) throw new Error("Test terminé");
+        if(i==3) throw new Error("Test terminé");
     }
     catch (e) {
         btn.removeEventListener("click", clic);
         btn.removeEventListener("click", tot);
-        btn.innerHTML ="Votre moyenne est de: "+Math.round(moy/1)+" ms";
         console.log("Test terminé ! Vous avez fait "+i+" essais.");
-        
+        let score=Math.round(moy/3);
+        let ch;
+        if(score<=400){
+            ch="Réflexe incroyable!";
+        }
+        else if(400<score<=800){
+            ch="Réflexe moyen";
+        }
+        else{
+            ch="Réflexe lent";
+        }
+        btn.innerHTML ="Votre moyenne est de: "+score+" ms<br>"+ch;
+        btn.style.backgroundColor = "rgb(106, 59, 2)";
         tm1 = 0;
         i=0;
         moy=0;
-        btn.style.backgroundColor = "rgba(101, 177, 238, 1)";
-        btn.style.padding = "70px 160px";
+        
     }
 }
